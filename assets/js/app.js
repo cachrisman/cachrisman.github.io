3cab215d79753687015e4349922dd24bee83b39d
Charlie Chrisman <charlie.chrisman@gmail.com>
2015-07-28 16:53:16 -0700

    fixed javascript close modal link reference v2

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,11 @@ window.onload = function () {
   document.body.className = '';
   document.addEventListener( "keyup", function ( e ) {
     if ( e.keyCode === 27 ) {
-      location.href = "/#"
+      location.href = "#"
     }
   } );
   document.getElementById( 'resume' ).addEventListener( "click", function ( e ) {
-    location.href = "/#"
+    location.href = "#"
   } );
 }
 
