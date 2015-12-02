cd086553b51bff1d023bb18b7c755873894b74ac
Charlie Chrisman <charlie.chrisman@gmail.com>
2015-12-01 16:16:31 -0800

    testing

--- test/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-  if (isset($_REQUEST['inspect'])) {
-    var_dump($_SERVER);
-  } else {
-    header('Access-Control-Allow-Origin: http://' . $_SERVER['HTTP_HOST']);
-    header('Content-type: text/plain');
-    readfile('app.js');
-  }
-?>
